82ad9a2a70aab04a6ed8659b3ca17aabd4cb0b34
Visvaldas <visva.rapalis@gmail.com>
2020-08-27 17:22:00 +0300

    Add tags functionality

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,17 @@
 class TagsController < ApplicationController
+  def show
+    @tags = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tags = Tag.find(params[:id])
+    @tags.destroy
+    flash.notice = "Tag '#{@tags.name}' Deleted!"
+    redirect_to action: 'index'
+  end
+  
 end
