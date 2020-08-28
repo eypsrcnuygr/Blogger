89105ed99a3551bc3754210eab25b60d695c0ccc
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-08-28 15:27:53 +0300

    Fix linter issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_08_28_093147) do
-
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -56,5 +55,4 @@ ActiveRecord::Schema.define(version: 2020_08_28_093147) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
